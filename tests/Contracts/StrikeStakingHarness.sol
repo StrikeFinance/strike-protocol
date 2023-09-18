e392e7cc5af8b8fd39c0406bd9bfc1e21bb5479f
dragonknight0801 <bluegem223@gmail.com>
2023-09-18 11:02:24 +0000

    fix staking harness

--- tests/Contracts/StrikeStakingHarness.sol
+++ tests/Contracts/StrikeStakingHarness.sol
@@ -5,7 +5,32 @@ pragma experimental ABIEncoderV2;
 import "../../contracts/Staking/StrikeStaking.sol";
 
 contract StrikeStakingHarness is StrikeStaking {
-    
+
+    address public blacklistedUser;
+
+    function setBlacklist(address account) public {
+        blacklistedUser = account;
+    }
+
+    /**
+     * @dev Prevents blacklisted users from calling the contract.
+     */
+    modifier nonBlacklist(address account) {
+        require(
+            account != blacklistedUser,
+            "Blacklisted"
+        );
+        _;
+    }
+
+    modifier onlyBlacklist(address account) {
+        require(
+            account == blacklistedUser,
+            "No blacklisted"
+        );
+        _;
+    }
+
     constructor() StrikeStaking() public {}
 
 }
